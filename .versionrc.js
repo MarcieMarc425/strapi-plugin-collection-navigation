afc2adefa6ab101062bfcda01f277402c635e8de
Marc Tse <marc.tse@axa.com.hk>
2022-08-27 00:22:02 +0800

    build: add standard-version for versioning

---
+++ .versionrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  // Files to check for version
+  packageFiles: [
+    {
+      filename: "package.json",
+      type: "json",
+    },
+  ],
+  // Files to bump version
+  bumpFiles: [
+    {
+      filename: "package.json",
+      type: "json",
+    },
+  ],
+  types: [
+    {
+      type: "feat",
+      section: "Features 🎯",
+    },
+    {
+      type: "fix",
+      section: "Bug Fixes 🐛",
+    },
+    {
+      type: "chore",
+      section: "Chores 🧹",
+    },
+    {
+      type: "docs",
+      section: "Documentation 📖",
+    },
+    {
+      type: "style",
+      section: "Styles 💅",
+    },
+    {
+      type: "refactor",
+      section: "Refactor 🔄",
+    },
+    {
+      type: "perf",
+      section: "Performance 🏎",
+    },
+    {
+      type: "test",
+      section: "Tests 🧪",
+    },
+    {
+      type: "build",
+      section: "Build System 🛠",
+    },
+    {
+      type: "ci",
+      section: "Continuous Integration 🚀",
+    },
+    {
+      type: "revert",
+      section: "Reverts ⏪",
+    },
+  ],
+};
